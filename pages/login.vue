c597604d165e6a6d51b1af42dd5e33d85b6f38e3
ztao <649949135@qq.com>
2024-06-16 00:02:00 +0800

    feat: 登录页定位完成

--- pages/login.vue
+++ pages/login.vue
@@ -1,7 +1,9 @@
 <script lang="ts" setup></script>
 
 <template>
-  <div class="bg-blue-400 box-border">登录页</div>
+  <div class="w-full h-full flex justify-center items-center">
+    <div class="bg-white w-96 h-96">登录/注册页</div>
+  </div>
 </template>
 
 <style lang="scss" scoped></style>
