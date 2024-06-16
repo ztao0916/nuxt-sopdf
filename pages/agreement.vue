bb507bb53d7616e5c2b2dd8cf0b39ab0710c75e5
ztao <649949135@qq.com>
2024-06-16 10:28:38 +0800

    feat: 用户协议页面完成

---
+++ pages/agreement.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="w-[800px] bg-white mt-5 h-fit p-2 box-border">
+    <p class="font-bold text-lg text-center leading-10">SOPDF用户协议:</p>
+    <p class="indent-8 break-all text-base">
+      SOPDF.CN（以下简称“本站”）是由工业从业者和爱好者自发创建的内容分享平台，是工业产品PDF/手册/样本/样册/型录/Catalogs/Datasheets（以下简称“PDF样册”）的内容搜索网站。本站收集各类工业产品的PDF样册，对PDF样册内容进行数据梳理，进而为各类研发工程师、设计工程师、科研人员、高校师生及行业从业者提供高效的产品内容检索服务。
+    </p>
+    <p class="indent-8 break-all text-base">
+      任何使用本站服务的个人或企业（以下简称“您”），应认真阅读本“SOPDF您协议”（以下简称“本协议”）的所有条款。自您使用本站服务之时起，即被认为同意本协议中的所有条款。
+    </p>
+    <p class="break-all text-base">1 隐私条款</p>
+    <p class="break-all text-base">
+      1.1
+      根据《中华人民共和国网络安全法》、《互联网您账号名称管理规定》等相关法律、法规之规定，您在使用本站的会员功能（如收藏、下载等）时应首先进行实名制注册。
+    </p>
+    <p class="break-all text-base">
+      1.2
+      您注册时应提供属于本人的手机号码。本站存储您的手机号码用于识别您的身份以便更好的为您提供个人化的服务。本站将采用当前技术环境下所能获得的必要技术手段以保护您的的个人隐私，不会在未经您授权情况下，以任何形式透露您的个人信息给第三方，除非有下列情况：
+    </p>
+    <p class="break-all text-base">
+      （1） 有关法律规定或本站的合法服务程序规定；
+    </p>
+    <p class="break-all text-base">
+      （2） 在紧急情况下，为维护您及公众的权益；
+    </p>
+    <p class="break-all text-base">
+      （3） 为维护本站的商标权、专利权及其他任何合法权益；
+    </p>
+    <p class="break-all text-base">
+      （4） 其他依法需要公开、编辑或透露个人信息的情况。
+    </p>
+    <p class="break-all text-base">2 知识产权条款</p>
+    <p class="break-all text-base">
+      2.1
+      本站刊载的所有数据内容，均经过本站运营人员加工和处理，为本站的数据资产，未经本站书面许可，任何个人或机构不得引用、复制、转载、摘编、镜像或以其他方式使用本站的上述全部或部分内容。对于有上述行为者，本站将依法追究其法律责任。
+    </p>
+    <p class="break-all text-base">
+      2.2
+      本站的程序代码为本站的知识产权，未经本站书面许可，任何个人或机构不得引用、复制、转载、摘编、做镜像、逆向编译或以其他方式使用本站的上述全部或部分内容。对于有上述行为者，本站将依法追究其法律责任。
+    </p>
+    <p class="break-all text-base">3 免责条款</p>
+    <p class="break-all text-base">
+      3.1
+      本站刊载的PDF样册之产品内容为专业的技术资料，相关内容来自互联网或由PDF样册的持有人或所在企业提供。本站不对内容的合理性、准确性、完整性和合规性负责。如相关PDF样册存在错误、遗漏、缺失或涉嫌侵犯您或您所在企业的合法权益，请通过合规渠道通知本站进行修正、下架或删除处理。
+    </p>
+    <div class="break-all text-base">
+      3.2
+      本站将采用当前技术环境下所能获得的必要技术手段以确保服务的稳定性和可持续性，然而依然可能会因程序代码存在疏漏、云服务器设备故障或失灵、人为操作疏忽、或遭第三人侵入系统篡改等因素，造成而全部或部分服务中断或延迟，对此您应予以理解和接受。
+    </div>
+    <div class="break-all text-base">
+      3.3
+      本站将采用当前技术环境下所能获得的必要技术手段以确保服务的稳定性和可持续性，然而依然可能会本站可能因黑客攻击、计算机病毒侵入或发作、政府管制而造成的暂时性关闭，或因前述原因导致您的个人资料泄露、丢失、被盗用或被篡改等，对此您应予以理解和接受。
+    </div>
+    <div class="break-all text-base">
+      3.4
+      本站有权在任何时候，修改、暂停或终止本平台全部或部分服务，对此您应予以理解和接受。
+    </div>
+    <div class="break-all text-base">4 其他条款</div>
+    <div class="break-all text-base">
+      4.1 本协议的标题仅为方便而设，在解释本协议时应被忽略。
+    </div>
+    <div class="break-all text-base">
+      4.2 本站保留对上述所有条款及未尽事项的最终解释权。
+    </div>
+    <div class="break-all text-base text-right">SOPDF.CN 2024年2月14日</div>
+  </div>
+</template>
+
+<style lang="css" scoped></style>
--- pages/login.vue
+++ pages/login.vue
@@ -1,8 +1,43 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+  const selected = ref(true);
+</script>
 
 <template>
   <div class="w-full h-full flex justify-center items-center">
-    <div class="bg-white w-96 h-96">登录/注册页</div>
+    <div
+      class="bg-white w-80 h-60 flex flex-col p-4 box-border justify-between"
+    >
+      <div class="login-title">
+        <span>登录</span>
+        <span>|</span>
+        <span>注册</span>
+      </div>
+      <div class="login-input border rounded-sm">
+        <UInput
+          placeholder="请输入手机号"
+          class="w-full"
+          color="white"
+          variant="none"
+        />
+      </div>
+      <div class="login-input border rounded-sm">
+        <UInput
+          placeholder="请输入验证码"
+          class="w-full"
+          color="white"
+          variant="none"
+        />
+      </div>
+      <div class="login-agreement">
+        <UCheckbox :model-value="selected" color="orange">
+          <template #label>
+            <span> 我已阅读并同意</span>
+            <NuxtLink class="text-sopdf-100" to="/agreement">用户协议</NuxtLink>
+          </template>
+        </UCheckbox>
+      </div>
+      <div class="login-submit">登录/注册</div>
+    </div>
   </div>
 </template>
 
